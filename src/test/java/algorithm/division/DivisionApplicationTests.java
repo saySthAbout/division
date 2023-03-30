1bec5108731d6b4b5ffee2712765c6bd8197c7ef
saySthAbout <saysthabout@gmail.com>
2023-03-30 17:02:36 +0900

    프로젝트 생성

---
+++ src/test/java/algorithm/division/DivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package algorithm.division;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
